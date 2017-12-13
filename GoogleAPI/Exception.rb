03ec9974304948dce1fc92173cafb64225191b18
apurvaK-Cuelogic <apurva.kirti@cuelogic.com>
2017-12-13 16:40:29 +0530

    GoogleAPI uploaded successfully

--- GoogleAPI/Exception.rb
+++
@@ -1,9 +0,0 @@
-def log_errors
-  File.open('errors.txt', 'a') do |file|
-    (Thread.current[:errors] ||= []).each do |error|
-
-      file.puts error
-    end
-  end
-end
-log_errors
--- GoogleAPI/Programs/UserView.rb
+++
@@ -1,8 +0,0 @@
-require_relative 'GoogleAPI'
-class UserView
-  def dispUserView
-    puts "Enter keyword you want to search on google"
-    keyword=gets.chomp
-    return keyword
-  end
-end
